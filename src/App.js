5dfdc7ff49e10a4a4ac4e1c2fb0bfb82d4040298
vilmelhina <90849312+vilmelhina@users.noreply.github.com>
2022-12-03 19:03:58 +0100

    Implemented recoil

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import Game from "./presenter/gamePresenter";
 import React from "react";
+import { RecoilRoot } from 'recoil';
 
 export default
 function App() {
   return (
-    <div className="App">
-        <Game/>
-    </div>
+      <RecoilRoot>
+        <div className="App">
+            <Game/>
+        </div>
+      </RecoilRoot>
   );
 }
 
