336728e105e042b60c0ecc6c67db4604bbce0581
Kevin O'Flaherty <kevinheltenkelt@gmail.com>
2022-12-06 10:45:13 +0100

    firebaseModel.js online and writes to database realtime database correctly.

--- src/integration/firebase/firebaseModel.js
+++ src/integration/firebase/firebaseModel.js
@@ -1,34 +1,17 @@
 import firebaseConfig from "./firebaseConfig";
 
-const firebase= require("firebase/app");
-require('firebase/database');
-
-window.firebase=firebase;
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from 'firebase/app';
+import {getDatabase, ref, set} from "firebase/database";
 // TODO: Add other SDKs for Firebase products that we want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 //till exempel:
-import { getDatabase, ref, set} from "firebase/database";
+//import { getAnalytics } from "firebase/analytics";
 
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const database = getDatabase(app);
+const fireBDataB = getDatabase();
 
-
-//TODO try to write to database
-const REF = "mainDB";
-
-export function writeToFirebaseACB(somethingToWrite   ){
-    console.log(analytics);
-    set(ref(database, REF), {kevinsCommentCorner : somethingToWrite})
-
-
-    //neither solution seems to work
-    firebase.database().ref(REF+"/kevinsCommentCorner").set(somethingToWrite);
-
+export function dbWriteTester(messageToPost) {
+    set(ref(fireBDataB, 'kevinsCommentCorner/'), {
+        message: messageToPost
+    });
 }
