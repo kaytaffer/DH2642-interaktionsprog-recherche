b25060d7f947ac6900d2510ad023f0cb80c81484
Kevin O'Flaherty <kevinheltenkelt@gmail.com>
2022-12-06 10:50:29 +0100

    added comments

--- src/integration/firebase/firebaseModel.js
+++ src/integration/firebase/firebaseModel.js
@@ -7,11 +7,16 @@ import {getDatabase, ref, set} from "firebase/database";
 //till exempel:
 //import { getAnalytics } from "firebase/analytics";
 
+//initializes firebase
 const app = initializeApp(firebaseConfig);
 const fireBDataB = getDatabase();
 
+//TODO: Add more functions for model -> persistence
+//A dummy write function for testing writing to the db
 export function dbWriteTester(messageToPost) {
     set(ref(fireBDataB, 'kevinsCommentCorner/'), {
         message: messageToPost
     });
 }
+
+//TODO: Add more functions for persistence -> model
