5c974d89c066bc94f595a1eb5c7b6ca043fb8d68
vilmelhina <90849312+vilmelhina@users.noreply.github.com>
2022-12-07 14:12:57 +0100

    Prepared functionality for incorrect words, limited score to 2 decimals, added key to definition in list

--- src/presenter/scoreBoardPresenter.js
+++ src/presenter/scoreBoardPresenter.js
@@ -3,12 +3,13 @@ import React from "react";
 
 import ScoreBoardView from "../view/scoreBoardView.js";
 import {useRecoilValue} from "recoil";
-import {definitionState, enteredWordsWithFrequencyState, givenWordState} from "../model/atoms.js";
+import {definitionState, enteredWordsWithFrequencyState, givenWordState, incorrectWordsState} from "../model/atoms.js";
 
 function ScoreBoard() {
     const givenWord = useRecoilValue(givenWordState);
     const definition = useRecoilValue(definitionState);
     const userWords = useRecoilValue(enteredWordsWithFrequencyState);
+    const incorrectUserWords = useRecoilValue(incorrectWordsState);
 
     return <ScoreBoardView word = {givenWord}
                     definition = {definition}
