30ad1a12a8eade128d9d84f05f400a0cfb89e981
vilmelhina <90849312+vilmelhina@users.noreply.github.com>
2022-12-15 15:26:01 +0100

    Links moved from hamburger menu view to its presenter as children

--- src/presenter/hamburgerMenuPresenter.js
+++ src/presenter/hamburgerMenuPresenter.js
@@ -1,11 +1,17 @@
 import React from "react";
 import HamburgerMenuView from "../view/hamburgerMenuView";
+import { Link } from "react-router-dom";
 
 function HamburgerMenu() {
 
     return (
         <div>
-            <HamburgerMenuView/>
+            <HamburgerMenuView>
+                <Link to="/">Start</Link>
+                <Link to="/about">About</Link>
+                <Link to="/rules">Rules</Link>
+                <Link to="/highscore">High Scores</Link>
+            </HamburgerMenuView>
         </div>
     )
 }
