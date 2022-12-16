407e3ed5fa8d43e4e597dca453eefdd4b66e0d91
vilmelhina <90849312+vilmelhina@users.noreply.github.com>
2022-12-16 11:54:35 +0100

    Changed colors (as variables, can be easily changed), fonts, and some layout changes. Some classnames edited. Moved some things around on scoreboard.

--- src/view/startScreenView.js
+++ src/view/startScreenView.js
@@ -5,6 +5,8 @@ function StartScreenView(props){
     }
     return (
         <div id="start">
+            <h1>Welcome</h1>
+            <p className="instruction">Recherché is a game about finding the most rare synonyms to a given word.</p>
             <button className="button" onClick={buttonClickedACB}>Start game</button>
         </div>
     );
