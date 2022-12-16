3b916dcbc78ee41ec9528c1f254923364262a014
Kevin O'Flaherty <kevinheltenkelt@gmail.com>
2022-12-16 11:07:27 +0100

    Refactoring according to separation of concerns persistence/model. Bugfixed an error (400).

--- src/integration/firebase/firebaseModel.js
+++ src/integration/firebase/firebaseModel.js
@@ -1,22 +1,58 @@
-//TODO: Deprecated file
-
-
 import firebaseConfig from "./firebaseConfig";
 
 import { initializeApp } from 'firebase/app';
-import {getDatabase, ref, set} from "firebase/database";
+import {getDatabase, ref, get, set, onValue} from "firebase/database";
 import {getAuth, signInAnonymously} from "firebase/auth";
 // TODO: Add other SDKs for Firebase products that we want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 //till exempel:
 //import { getAnalytics } from "firebase/analytics";
 
+//FIREBASE SETUP
 //initializes firebase and relevant SDK:s
 const app = initializeApp(firebaseConfig);
-const fireBDataB = getDatabase();
-const authentication = getAuth();
+export const fireBDataB = getDatabase();
+//const authentication = getAuth();
 
 
+
+// ACB updates atom value according to changes in firebase given a snapshot of the database state
+function onDatabaseChangeACB(snapshot) {
+    if (snapshot.exists()) {
+        return {highScoreNames: snapshot.val().highScoreNames,
+            highScores: snapshot.val().highScores}
+    } else {
+        console.log("When trying to find persistent high score no data was available");
+    }
+}
+
+export function checkEmptyFirebaseDBPath(databasePath){
+    function treatSnapshotACB(snapshot){
+        return onDatabaseChangeACB(snapshot)
+    }
+    return get(ref(fireBDataB, databasePath)).then(treatSnapshotACB).catch((error) => {
+        console.error(error);
+    })
+}
+
+export function subscribeToDBPath(databasePath) {
+    onValue(ref(fireBDataB, databasePath), onDatabaseChangeACB);
+}
+
+export function unsubscribeToDBPath (databasePath) {
+    onValue(ref(fireBDataB, databasePath), null);
+}
+
+//On changes in the atom, updates the persistent database.
+export function onLocalChange(databasePath, highScoreObject){
+    return set(ref(fireBDataB, databasePath), {
+        highScoreNames: Object.assign({}, highScoreObject.highScoreNames),
+        highScores: Object.assign({}, highScoreObject.highScores)}).catch(error => {
+        console.error(error);
+    })
+}
+
+/* TODO authentication:
 signInAnonymously(authentication)
     .then(() => {
         // Signed in..
@@ -26,19 +62,5 @@ signInAnonymously(authentication)
         const errorMessage = error.message;
         // ...
     });
+*/
 
-//TODO: Add more functions for model -> persistence
-
-
-
-export function scoreboardUpdate() {
-  //  const scoreNames = useRecoilValue(highScoreState).highScoreNames
-  //  const scorePoints = useRecoilValue(highScoreState).highScorePoints
-    return set(ref(fireBDataB, 'highScore/'), {
-        highScoreNameList: Object.assign({}, scoreNames),
-        highScorePointsList: Object.assign({}, scorePoints)
-    });
-}
-
-
-//TODO: Add more functions for persistence -> model§
