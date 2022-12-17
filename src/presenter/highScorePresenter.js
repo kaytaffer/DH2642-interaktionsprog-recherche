7d82862953a30aa0d5b1c57c81b16b3abe7e85a5
vilmelhina <90849312+vilmelhina@users.noreply.github.com>
2022-12-17 11:18:35 +0100

    Added button in top bar to exit game. Removed button in scoreboard to exit game. Removed some commented out code.

--- src/presenter/highScorePresenter.js
+++ src/presenter/highScorePresenter.js
@@ -6,16 +6,6 @@ import {highScoreState} from "../model/persistenceAtoms";
 function HighScore() {
     const [highScore, setHighScore] = useRecoilState(highScoreState)
 
-    // function combinedHighScores(){
-    //     function combineCB(element, index) {
-    //         return {name: element, score: highScore.highScores[index]}
-    //     }
-    //     return highScore.highScoreNames.map(combineCB);
-    // }
-
-    // set(ref(fireBDataB, '/highScore'), [{name:'dogge',score:-1}]);
-    // get(ref(fireBDataB, '/highScore')).then(snapshot => {console.log(snapshot.val())})
-
     console.log(highScore);
 
     function test(){
