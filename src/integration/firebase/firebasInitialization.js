cd175a50bfb7350f4358175d042f28c5d628c46d
Kevin O'Flaherty <kevinheltenkelt@gmail.com>
2022-12-18 14:22:16 +0100

    getAuth

--- src/integration/firebase/firebasInitialization.js
+++ src/integration/firebase/firebasInitialization.js
@@ -2,10 +2,8 @@ import firebaseConfig from "./firebaseConfig";
 
 import { initializeApp } from 'firebase/app';
 import {getDatabase} from "firebase/database";
-import firebaseui from "firebaseui";
+import { getAuth } from "firebase/auth";
 
-//let firebase = require('firebase');
-//let firebaseui = require('firebaseui');
 // TODO: Add other SDKs for Firebase products that we want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 //till exempel:
@@ -15,4 +13,19 @@ import firebaseui from "firebaseui";
 //initializes firebase and relevant SDK:s
 const app = initializeApp(firebaseConfig);
 export const fireBDataB = getDatabase();
-//export let authenticationUI = new firebaseui.auth.AuthUI(firebase.auth());
+export const fireBAuth = getAuth();
+
+
+/*TODO
+createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        // ...
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+    });
+ */
